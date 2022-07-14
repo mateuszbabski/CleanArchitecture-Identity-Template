2ae07d52082bfb5ea037170be81078e75a593563
mateuszbabski <babskimateusz@gmail.com>
2022-07-14 16:30:27 +0200

    ServiceExtensions for Infrastructure updated

---
+++ Infrastructure/ServiceExtension.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Settings;
+using Infrastructure.Identity.Context;
+using Infrastructure.Identity.Model;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class ServiceExtension
+    {
+        public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<IdentityContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                b => b.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName)));
+
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+
+            services.Configure<JWTSettings>(configuration.GetSection("Authentication"));
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = configuration["Authentication:Issuer"],
+                        ValidAudience = configuration["Authentication:Audience"],
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Authentication:Key"]))
+                    };
+                });
+
+
+
+
+        }
+    }
+}
